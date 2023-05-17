7d1716dcc3c9fe0d5669bf0e57e85a337496c182
nurullah hilcan <128026157+nrlhhlcn@users.noreply.github.com>
2023-05-17 15:44:03 +0300

    iki faktoryel sayinin farki

--- faktoryel.py
+++ faktoryel.py
@@ -1,10 +1,17 @@
-def faktoryel(sayi): # faktoryel hesaplayan fonksiyon
-    fak=1
-    if sayi<1:
+def faktoryel(sayi1,sayi2): # faktoryel hesaplayan fonksiyon
+    fak1=1
+    fak2=1
+    if sayi1<1 or sayi2<1:
         print("hatalı veri girişi")
     else:
-        for i in range(1,sayi+1):
-            fak*=i
-        print(f"{sayi}!:",fak)
-sayi=int(input("bir sayi giriniz:"))
-faktoryel(sayi)
+        for i in range(1,sayi1+1):
+            fak1*=i
+        for i in range(1,sayi2+1):
+            fak2*=i
+
+
+
+        print(f"{sayi1}!-{sayi2}!:",fak1-fak2)
+sayi1=int(input("birinci sayi giriniz:"))
+sayi2=int(input("ikinci sayi giriniz:"))
+faktoryel(sayi1,sayi2)
